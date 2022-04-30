cd23ebc3ec8cc35039df49c17acccad0fe249f68
Abinet Tamiru <Abinettamiru@outlook.com>
2022-04-30 22:35:14 +0300

    Add project files.

---
+++ Csharp project Q11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_project_Q11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gravity_of_earth = 9.807;
+            double gravity_of_moon = gravity_of_earth * 0.17;
+            Console.Write("This program will calculate your weight in MOON\ninsert your mass: ");
+            double x = double.Parse(Console.ReadLine()) * gravity_of_moon;
+            Console.WriteLine("Your weight in MOON is: " + x);
+
+
+        }
+    }
+}
